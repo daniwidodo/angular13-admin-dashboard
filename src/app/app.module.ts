9b2440b6961dbc120c62cb1407dbf527ed1a8530
bangtur_studio <dani.widodo87@gmail.com>
2022-02-02 06:17:30 +0800

    initial upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
+import { NavigationBarComponent } from './navigation/navigation-bar/navigation-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavigationBarComponent,
+    DashboardComponent,
+   
   ],
   imports: [
     BrowserModule
